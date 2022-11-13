9f1fcd16635c103e41a646c9ccb6fd878a882bda
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-11-13 10:13:26 +0700

    feat: update local font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
 import "@styles/globals.css"
 import type { AppProps } from "next/app"
-import { Poppins } from "@next/font/google"
-
-const poppins = Poppins({ weight: ["400", "500", "600", "700"] })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={poppins.className}>
       <Component {...pageProps} />
-    </main>
   )
 }
 
