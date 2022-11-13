b31c7d13642e60befa68b70394a4a5991be81455
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-11-13 09:07:51 +0700

    feat: remove yarn lock

--- next.config.js
+++ next.config.js
@@ -2,29 +2,28 @@
 const nextConfig = {
     reactStrictMode: true,
     experimental: { appDir: true },
-    env: {
-        serviceBackend: "http://localhost:8800", // example config port Backend
-    },
+    // env: {
+    //     serviceBackend: "http://localhost:8800", // example config port Backend
+    // },
     // assetPrefix: isProduction ? "" : "http://localhost:3000",
-    i18n: {
-        locales: ["id", "en", "fr", "de", "au", "uk"],
-        defaultLocale: "en",
-    },
+    // i18n: {
+    //     locales: ["id", "en", "fr", "de", "au", "uk"],
+    //     defaultLocale: "en",
+    // },
     swcMinify: true,
-    reactStrictMode: true,
-    images: {
-        domains: ["tailwindui.com", "lh3.googleusercontent.com", "imdb-api.com", "m.media-amazon.com", "i.gr-assets.com", "s0.bukalapak.com", "placeimg.com", "pragprog.com", "ui-avatars.com", "flagcdn.com", "cdn-images-1.medium.com"],
-        dangerouslyAllowSVG: true,
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    },
+    // images: {
+    //     domains: ["tailwindui.com", "lh3.googleusercontent.com", "imdb-api.com", "m.media-amazon.com", "i.gr-assets.com", "s0.bukalapak.com", "placeimg.com", "pragprog.com", "ui-avatars.com", "flagcdn.com", "cdn-images-1.medium.com"],
+    //     dangerouslyAllowSVG: true,
+    //     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    // },
     // example rewrites url when path is matching
-    async rewrites() {
-        return [{
-            source: "/healthyme/",
-            destination: "https://healthyme.com"
-        }]
-    },
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    // async rewrites() {
+    //     return [{
+    //         source: "/healthyme/",
+    //         destination: "https://healthyme.com"
+    //     }]
+    // },
+    // pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 }
 
 export default nextConfig
