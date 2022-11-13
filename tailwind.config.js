66d19d9b9bb055c061742b6950e9662acd79a178
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-11-13 10:39:57 +0700

    feat: update local font

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,7 @@ module.exports = {
                 activeState: "8px 8px 40px rgba(12, 27, 58, 0.06)"
             },
             fontFamily: {
-                "gilroy-bold": ["gilroy-bold", "sans-serif"],
-                "gilroy-semibold": ["gilroy-semibold", "sans-serif"],
-                "gilroy-medium": ["gilroy-medium", "sans-serif"],
-                "gilroy-normal": ["gilroy-normal", "sans-serif"],
-                "gilroy-light": ["gilroy-light", "sans-serif"],
-                "gilroy-extralight": ["gilroy-extralight", "sans-serif"],
-                "gilroy-thin": ["gilroy-thin", "sans-serif"]
+                "Gilroy": ["Gilroy", "sans-serif"]
             },
             screens: {
                 iphone: { min: "300px", max: "767px" },
