dcd3560601d316ef18c1c1ac684831a2ca6d76b9
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-11-13 09:42:43 +0700

    add next font poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import "@styles/globals.css"
 import type { AppProps } from "next/app"
-import { Inter } from "@next/font/google"
+import { Poppins } from "@next/font/google"
 
-const inter = Inter()
+const poppins = Poppins({ weight: ["400", "500", "600", "700"] })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main className={poppins.className}>
       <Component {...pageProps} />
     </main>
   )
