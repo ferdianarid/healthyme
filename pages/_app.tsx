398d5dde7fd3b6c7feb967ca35021194170466ae
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-11-13 10:26:32 +0700

    add config custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,45 @@
 import "@styles/globals.css"
 import type { AppProps } from "next/app"
+import localFont from "@next/font/local"
+
+const customFonts = localFont({
+  src: [
+    {
+      path: "../fonts/Gilroy-Bold.woff",
+      style: "normal",
+      weight: "700"
+    },
+    {
+      path: "../fonts/Gilroy-Semibold.woff",
+      style: "normal",
+      weight: "600"
+    },
+    {
+      path: "../fonts/Gilroy-Medium.woff",
+      style: "normal",
+      weight: "500"
+    },
+    {
+      path: "../fonts/Gilroy-Regular.woff",
+      style: "normal",
+      weight: "400"
+    }
+  ],
+  fallback: ["Gilroy", "sans-serif"]
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+      <style jsx global>
+        {`
+        :root {
+          --font-base: "${customFonts.style.fontFamily}"
+        }
+      `}
+      </style>
       <Component {...pageProps} />
+    </>
   )
 }
 
